541a99c0d9f9f228d526ec70dbab75322c1c9291
GodRyosuke <ienvs0828@outlook.jp>
2020-11-25 17:10:12 +0900

    google console google analyticsの設定

--- header.php
+++ header.php
@@ -3,12 +3,20 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?php bloginfo('description'); ?>">
     <title><?php bloginfo('name'); ?></title>
     <script src="https://kit.fontawesome.com/29d47546b5.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <script src="<?php echo get_template_directory_uri() ?>/assets/js/main.js" defer></script>
     <?php wp_head(); ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-EX1J658Q79"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-EX1J658Q79');
+    </script>
   </head>
   <body>
     <header class="bg-light">
